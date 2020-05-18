3dc903a9becd743aa8e5956a02d44ad9f8605c2d
jcrn <clemons.jason@gmail.com>
2020-05-18 11:19:22 -0700

    added initial api call and initial structuring

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import './App.css'
 
 function App() {
+  const [comic, setComic] = useState(null)
+
+  useEffect(() => {
+    axios
+      .get('https://cors-anywhere.herokuapp.com/http://xkcd.com/info.0.json')
+      .then((res) => setComic(res.data))
+      .catch((err) => console.log(err))
+  }, [])
+
+  const fetchComic = (num) => {
+    setComic(null)
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/http://xkcd.com/${num}/info.0.json`
+      )
+      .then((res) => setComic(res.data))
+      .catch((err) => console.log(err))
+  }
+
+  if (!comic) {
+    return <div>loading...</div>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {comic.title}
+      <button onClick={() => fetchComic(comic.num - 1)}>previous</button>
+      <img src={comic.img} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
